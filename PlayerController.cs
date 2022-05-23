91f2744603b9a7f9790dc615ef1fa2e3246648eb
Isaac Emilien <isaac.emilien@gmail.com>
2022-05-23 16:58:10 +0100

    Player controller changes

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
     #region FIELDS
     Rigidbody2D rb;
-
+    [SerializeField] float lol = 180f;
     [SerializeField] float speed = 1;
 
     bool itemGrabbed = false;
@@ -22,6 +23,10 @@ public class PlayerController : MonoBehaviour
 	
     void Update()
     {
+        Vector2 currentPos = gameObject.transform.position;
+
+        // gameObject.transform.position = new Vector2(currentPos.x + Input.GetAxis("Horizontal") / moveSlowness, currentPos.y + Input.GetAxis("Vertical") / moveSlowness);
+
         if (Input.GetKey(KeyCode.W))
         {
             rb.MovePosition(rb.position + Vector2.up * speed * Time.fixedDeltaTime);
@@ -38,6 +43,15 @@ public class PlayerController : MonoBehaviour
         {
             rb.MovePosition(rb.position + Vector2.right * speed * Time.fixedDeltaTime);
         }
+
+        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A)){
+            rb.MovePosition(rb.position + VectorFromAngle(lol) * speed * Time.fixedDeltaTime);
+        }
+
+        // Debug.Log(Math.Abs(VectorFromAngle(60).x) + " " + Math.Abs(VectorFromAngle(60).y));
+        Debug.Log(VectorFromAngle(lol));
+
+
     }
     #endregion
 
@@ -51,6 +65,15 @@ public class PlayerController : MonoBehaviour
 
             Debug.Log("Item Collected");
         }
+    }
+
+    Vector2 VectorFromAngle(float theta)
+    {
+        return new Vector2(Mathf.Cos(theta), Mathf.Sin(theta)); // Trig is fun
+    }
+
+    Vector2 AbsVector2(Vector2 v2){
+        return new Vector2(Math.Abs(VectorFromAngle(60).x), Math.Abs(VectorFromAngle(60).y));
     }
     #endregion
 }

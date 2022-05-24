1193a9cdac459824100fb634bd3a40fe50c5cb45
Isaac Emilien <isaac.emilien@gmail.com>
2022-05-24 10:15:46 +0100

    Reverted to GetAxis movement, cleaned code

--- PlayerController.cs
+++ PlayerController.cs
@@ -7,11 +7,9 @@ public class PlayerController : MonoBehaviour
 {
     #region FIELDS
     Rigidbody2D rb;
-    [SerializeField] float lol = 180f;
-    [SerializeField] float speed = 1;
+    [SerializeField] float moveSlowness = 60;
 
     bool itemGrabbed = false;
-    
 
     #endregion
 
@@ -25,40 +23,12 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 currentPos = gameObject.transform.position;
 
-        // gameObject.transform.position = new Vector2(currentPos.x + Input.GetAxis("Horizontal") / moveSlowness, currentPos.y + Input.GetAxis("Vertical") / moveSlowness);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            rb.MovePosition(rb.position + Vector2.up * speed * Time.fixedDeltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.MovePosition(rb.position + Vector2.left * speed * Time.fixedDeltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.MovePosition(rb.position + Vector2.down * speed * Time.fixedDeltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.MovePosition(rb.position + Vector2.right * speed * Time.fixedDeltaTime);
-        }
-
-        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A)){
-            rb.MovePosition(rb.position + VectorFromAngle(lol) * speed * Time.fixedDeltaTime);
-        }
-
-        // Debug.Log(Math.Abs(VectorFromAngle(60).x) + " " + Math.Abs(VectorFromAngle(60).y));
-        Debug.Log(VectorFromAngle(lol));
-
-
+        gameObject.transform.position = new Vector2(currentPos.x + Input.GetAxis("Horizontal") / moveSlowness, currentPos.y + Input.GetAxis("Vertical") / moveSlowness);
     }
-    #endregion
 
-    #region METHODS 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.tag == "Item")
+        if (collision.tag == "Item")
         {
             itemGrabbed = true;
             collision.gameObject.SetActive(false);
@@ -66,14 +36,9 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Item Collected");
         }
     }
+    #endregion
 
-    Vector2 VectorFromAngle(float theta)
-    {
-        return new Vector2(Mathf.Cos(theta), Mathf.Sin(theta)); // Trig is fun
-    }
+    #region METHODS 
 
-    Vector2 AbsVector2(Vector2 v2){
-        return new Vector2(Math.Abs(VectorFromAngle(60).x), Math.Abs(VectorFromAngle(60).y));
-    }
     #endregion
 }
